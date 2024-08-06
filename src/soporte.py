3adae5a71c8fc34b3e4c78fcb0c6c9d0e98ee376
Bertha Karolina <b.karolinaguillen@gmail.com>
2024-08-06 10:26:22 +0200

    pair 11

---
+++ src/soporte.py
@@ -0,0 +1,13 @@
+#%%
+# importamos las librerías que necesitamos
+# Tratamiento de datos
+# -----------------------------------------------------------------------
+import pandas as pd
+import numpy as np
+# Imputación de nulos usando métodos avanzados estadísticos
+# -----------------------------------------------------------------------
+
+
+pd.set_option('display.max_columns', None) # para poder visualizar todas las columnas de los DataFrames
+# %%
+\
